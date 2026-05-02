7af9bccead70646faefab9a5a9006ac9215fa299
Magdum <pirjademl7@gmail.com>
2026-05-02 15:06:32 +0530

    Revert "feat : add dto objects for concerned github events"
    This reverts commit 1cac3b6e9c0236feefdacb309d1b60c94282a841.

--- dtos/owner.dto.go
+++
@@ -1,7 +0,0 @@
-package dtos
-
-type Owner struct {
-	Login     string `json:"login"`
-	Id        int    `json:"id"`
-	AvatarUrl string `json:"avatar_url"`
-}
--- dtos/sender.dtos.go
+++
@@ -1,6 +0,0 @@
-package dtos
-
-type Sender struct {
-	Login string `json:"login"`
-	Id    int    `json:"id"`
-}
