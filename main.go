96f02c8a0f73b39ce35bceaaec0d5e1d6df57320
Magdum <pirjademl7@gmail.com>
2026-05-09 20:44:19 +0530

    chore : remove reduandant logs and print statements

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/pirjademl/vento-bot/config"
 	"github.com/pirjademl/vento-bot/handlers"
@@ -13,10 +14,23 @@ import (
 	"github.com/pirjademl/vento-bot/services"
 )
 
+var (
+	SYSTEM_INSTRUCTION string = os.Getenv("PULL_REQUEST_SYSTEM_INSTRUCTION")
+)
+
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
 	config.LoadEnv()
+	systemInstruction := strings.ReplaceAll(
+		os.Getenv("PULL_REQUEST_SYSTEM_INSTRUCTION"),
+		`\n`,
+		"\n",
+	)
+	if systemInstruction == "" {
+		log.Fatal(" PULL_REQUEST_SYTEM_INSTRUCTION NOT FOUND !!")
+		return
+	}
 
 	conn, err := config.ConnectDB()
 	if err != nil {
@@ -40,7 +54,7 @@ func main() {
 	vectorService.CreatePayloadIndex()
 
 	if err != nil {
-		log.Fatalf("creating collection err", err)
+		log.Fatalf("creating collection err %s", err)
 	}
 
 	handler := handlers.NewHandler(dbLayer, vectorService)
